e42c9e99828c934cf5240d5572e5d9f5726c3a00
jaum_1502 <aquelecaraalto@gmail.com>
2024-06-21 17:58:16 -0300

    pog

---
+++ screen-match.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Bem vindo(a) ao screen match!!!\n" ;
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+$notaFilme  = 0.8 ;
+$incluidoNoPlano = true;
+
+echo $nomeFilme;
+echo "O nome do filme é: ". $nomeFilme;
