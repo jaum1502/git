2f6f78ca6a89c06fa1a39c0ccd1bebd5fd87bdd9
jaum_1502 <aquelecaraalto@gmail.com>
2024-08-02 17:58:45 -0300

    froggers

---
+++ teste/public/exporta-arquivo.php
@@ -0,0 +1,11 @@
+<?php
+
+$infos = [
+'nome'=> $_POST['nome'],
+'idade'=> $_POST  ['idade'],
+'cidade'=> $_POST ['cidade']
+];
+
+file_put_contents('sobre.json', json_encode($infos));
+
+header('location:/sucesso.php?nome='.$infos['nome'].$infos['idade']);
---
+++ teste/public/sucesso.php
@@ -0,0 +1,6 @@
+<h1> Bom dia 
+    <?php echo $_GET['nome'] ?>
+    , <?php echo $_GET['idade'] ?> anos, mora em
+    <?php echo $_GET['cidade'] ?>
+    </em>
+</h1>
